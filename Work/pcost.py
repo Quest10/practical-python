05041e1c08386d56eb9b22c9d8560f42daa023aa
Gerardo Diez Garcia <gerardo.diez.garcia@gmail.com>
2020-06-20 00:42:06 +0200

    Transformation into a function
    Point of backup. In this version we skip the header manually.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,21 +1,35 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.27, 1.30, 1.31
 
+# 1.27
 # The columns in portfolio.csv correspond to the stock name, number of shares, and purchase
 # price of a single stock holding. Write a program called pcost.py that opens this file, reads
 # all lines, and calculates how much it cost to purchase all of the shares in the portfolio.
 
-total_cost = 0
+# 1.30
+# Take the code you wrote for the pcost.py program in Exercise 1.27 and turn it into a function
+# portfolio_cost(filename). This function takes a filename as input, reads the portfolio data in
+# that file, and returns the total cost of the portfolio as a float.
 
-f = open('Data/portfolio.csv', 'rt')
-# Skipp headers
-headers = next(f).split(',')
-for line in f:
-    row = line.split(',')
-    # remember index start at 0
-    # strip for delete \n characters. 
-    total_cost = total_cost + ( int(row[1]) * float(row[2].strip()) )
+# 1.31
+# Modify the pcost.py program to catch the exception, print a warning message, and continue processing
+# the rest of the file.
 
-print(f'Total amount: {total_cost}$')
+
+def portfolio_cost(filename):
+    total_cost = 0
     
+    f = open(filename, 'rt')
+    # Skipp headers
+    headers = next(f).split(',')
+    for line in f:
+        row = line.split(',')
+        # remember index start at 0
+        # strip for delete \n characters. 
+        total_cost = total_cost + ( int(row[1]) * float(row[2].strip()) )
+    f.close()    
+    return total_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total amount: {cost}$')   
